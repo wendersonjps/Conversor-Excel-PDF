ae48979b9c5b51f299b650da441455b94d0feffe
Wenderson Júnio <wendersonjps@gmail.com>
2023-02-15 14:30:49 -0300

    :hammer: Integration of classes

--- conversor/app.js
+++ conversor/app.js
@@ -1,11 +1,19 @@
 var Reader = require('./Reader')
+var Writer = require('./Writer')
 var Processor = require('./Processor')
+var Table = require('./Table')
+var HtmlParser = require('./HtmlParser')
 
 var reader = new Reader()
+var writer = new Writer()
 
 async function main() {
     var data = await reader.Read('./users.csv')
     var processedData = Processor.Process(data)
+    var users = new Table(processedData)
+    var html = await HtmlParser.Parse(users)
+
+    writer.Write(Date.now() + '.html', html)
 }
 
 main()
