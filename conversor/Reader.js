8e40e3c7a61db8b0c9c146b262de988e4ec712d7
Wenderson Júnio <wendersonjps@gmail.com>
2023-02-15 12:50:05 -0300

    :hammer: Created async function to read the file

--- conversor/Reader.js
+++ conversor/Reader.js
@@ -1,14 +1,17 @@
 const fs = require('fs')
+const util = require('util')
 
 class Reader {
-    Read(filepath) {
-        fs.readFile(filepath, 'utf-8', (err, data) => {
-            if (err) {
-                console.error(err)
-            } else {
-                console.log(data)
-            }
-        })
+    constructor() {
+        this.reader = util.promisify(fs.readFile)
+    }
+
+    async Read(filepath) {
+        try {
+            return await this.reader(filepath, 'utf-8')
+        } catch (err) {
+            return undefined
+        }
     }
 }
 
